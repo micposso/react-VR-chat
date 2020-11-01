14754837c18f9fcb73780e9e7da7c1d269595a49
unknown <micposso@gmail.com>
2020-11-01 10:06:23 -0500

    set events emmiter from chat component to socket io

--- client/src/components/Chat/ChatComponent.js
+++ client/src/components/Chat/ChatComponent.js
@@ -3,24 +3,31 @@ import React, { useState, useEffect } from 'react';
 // use to retrieve data from the URL
 import queryString from 'query-string';
 import io from 'socket.io-client';
+// set socket variable that will change
+let socket;
 
 const ChatComponent = ( {location} ) => {
   // use hook to pass url parameters in useEffect as state of the component
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  // set endpoint, server must be running
+  const ENDPOINT = 'localhost:5000';
+
 
   useEffect(() => {
     // get data from url
     // location comes from react router, and returns a prop
     // below returnt the url, but needs to be restructured into variables
     const {name, room} = queryString.parse(location.search);
+    // start a scoket from the endpoint
+    socket = io(ENDPOINT);
     //console.log(name, room);
     // pass the data from url saved in variables to the useState
     setName(name);
     setRoom(room);
-
-  });
-
+    console.log(socket);
+  }, [ENDPOINT, location.search]);
+  // above, need to pass an argument to userEffect to only run when parameters change, this way. socket only runs once
   return (
     <h1>Chat</h1>
   )
