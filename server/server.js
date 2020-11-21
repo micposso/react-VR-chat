f429a2ff426ceb79e8e89f046b30a524db5792eb
unknown <micposso@gmail.com>
2020-11-21 09:24:17 -0500

    pass adduser function and data objects

--- server/server.js
+++ server/server.js
@@ -15,17 +15,26 @@ const io = socketio(server);
 
 // use socketio to register user connect and disconnect, the connection is managed from inside the socket, now its ready to be implemented in the frontend
 io.on('connection', (socket) => {
+
   console.log("We have a new connection ");
   // listen for the event that is being emmited, reference by string
   socket.on('join', ({ name, room }, callback) => {
+    // error log
+    const { error, user } = AddUser({ id: socket.id, name, room });
+
+    if(error) return callback(error);
     // now we have access on the backend to this info
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}`});
+
+    // broadcast sends a message to everyone. use when user joins chat
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined`});
+
     console.log(name, room);
-    // trigger a response when the join event is detected
-    // do some error fake handling here
-    const error = false;
-    if(error){
-    callback({ error: 'error'});
-    }
+
+    socket.join(user.room);
+    // run the callback with no error
+    callback();
+
   });
 
   io.on('disconnect', () => {
