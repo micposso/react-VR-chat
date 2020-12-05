fc370dfec94c4e78ee88e01cd493223db73ce948
unknown <micposso@gmail.com>
2020-12-04 22:00:28 -0500

    add emoji component test to input component

--- client/src/components/Input/InputComponent.js
+++ client/src/components/Input/InputComponent.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Picker from 'emoji-picker-react';
+
 import './input.css';
 
-const Input = ({ message, setMessage, sendMessage }) => (
-  <form className="form">
+const Input = ({ message, setMessage, sendMessage }) => {
+
+  const [chosenEmoji, setChosenEmoji] = useState(null);
+
+  const onEmojiClick = (event, emojiObject) => {
+    setChosenEmoji(emojiObject);
+  };
+
+  return (
+    <form className="form">
     <input
     className="input"
     type="text"
@@ -11,8 +21,17 @@ const Input = ({ message, setMessage, sendMessage }) => (
     onChange={({target : {value}}) => setMessage(value) } 
     onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
     />
+        <div>
+      {chosenEmoji ? (
+        <span>You chose: {chosenEmoji.emoji}</span>
+      ) : (
+        <span>No emoji Chosen</span>
+      )}
+      <Picker onEmojiClick={onEmojiClick} />
+    </div>
     <button className="sendButton" onClick={(event) => sendMessage(event)}>SEND</button>
   </form>
-)
+  )
+};
 
 export default Input;
